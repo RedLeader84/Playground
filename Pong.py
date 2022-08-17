d24516c5192c3c0d5ea502478e26baf59426d736
Garrett B <92005543+RedLeader84@users.noreply.github.com>
2022-08-17 10:07:32 -0700

    Paddle b code inserted
    Inserted code for paddle b functionality

--- Pong.py
+++ Pong.py
@@ -8,6 +8,7 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Paddle A
+
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -18,4 +19,12 @@ paddle_a.goto(-350, 0)
 
 
 # Paddle B
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0)
 # Ball
