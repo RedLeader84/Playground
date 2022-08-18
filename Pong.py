37376a1f7ae16f07e97880e98e6af57f93ad9b84
Garrett B <92005543+RedLeader84@users.noreply.github.com>
2022-08-18 10:47:28 -0700

    Updated keyboard, Paddle B functionality

--- Pong.py
+++ Pong.py
@@ -40,12 +40,30 @@ ball.goto(0, 0)
 #Function
 def paddle_a_up():
     y= paddle_a.ycor()
-    y =+ 20
+    y += 20
     paddle_a.sety(y)
 
-#Keyboard
+def paddle_a_down():
+    y= paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+    
+    def paddle_b_up():
+    y= paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+    
+    def paddle_b_down():
+    y= paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+    
+#Keyboard Binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main Game Loop
 while True:
