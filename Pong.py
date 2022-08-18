458562eab6eaa91da59cd879115e7566a667b207
Garrett <garrettblankenship@DeathBook-Pro.local>
2022-08-18 11:49:58 -0700

    Border Checking

--- Pong.py
+++ Pong.py
@@ -11,6 +11,7 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Paddle A
+
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -30,14 +31,19 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 # Ball
+
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
+
 
 #Function
+
 def paddle_a_up():
     y= paddle_a.ycor()
     y += 20
@@ -59,6 +65,7 @@ def paddle_b_down():
     paddle_b.sety(y)
     
 #Keyboard Binding
+
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
@@ -66,5 +73,25 @@ wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
 # Main Game Loop
+
 while True:
     wn.update()
+
+    # Move the Ball
+
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    
+    # Border Checking 
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0)
+        ball.dx *= -1
