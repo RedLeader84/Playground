4c0bba6d05052205276e83b4219cf84a55d58b58
Garrett <garrettblankenship@DeathBook-Pro.local>
2022-08-19 12:17:19 -0700

    Added Pen functionality, paddle and ball collisions, sound effects and a scoring mechanism

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -10,6 +10,12 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Scoring Mechanism
+
+score_a = 0
+score_b = 0
+
+
 # Paddle A
 
 paddle_a = turtle.Turtle()
@@ -40,6 +46,16 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
+
+# Pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 
 #Function
@@ -87,11 +103,39 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system("afplay fart.wav&")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system("afplay fart.wav&")
 
     if ball.xcor() > 390:
-        ball.goto(0)
+        ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    # Paddle and Ball Collisions
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+        os.system("afplay fart.wav&")
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
+        os.system("afplay fart.wav&")
+
+        
+
