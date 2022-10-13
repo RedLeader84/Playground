de3036f13ef7890e7fd4c131101f9628e40b0859
Garrett <garrettblankenship@DeathBook-Pro.local>
2022-10-13 16:34:19 -0700

    New Weather App

---
+++ fundamentals/fundamentals.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+age = input("Whats your age? ")
+
+print("your name is " + name, "and your age is " + age)
+
+city = input("What city do you live in? ")
+state = input("What State do you live in? ")
+
+print("Your city is " + city, "and your state is " + state)
+
