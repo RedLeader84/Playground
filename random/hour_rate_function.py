e7adc3221178b9a82e7989eaebd9f18cfa8ef8d5
Garrett <garrettblankenship2002@gmail.com>
2023-04-06 11:32:28 -0700

    Hour Rate Function

---
+++ random/hour_rate_function.py
@@ -0,0 +1,13 @@
+    
+def computepay(h, r):
+    hrs = input("Enter Hours Worked:")
+    rate = float(input("Enter Rate of Pay:"))
+    h = float(hrs)
+    r = float(rate)
+    if h <= 40:
+        print(h*r)
+    else:
+        return (h-40)*(1.5*r)+(40*r)
+
+p = computepay(10, 20)
+print("Pay", p)
