dec23bdb0bdf64f614b538cbb2c09c474708c09c
Garrett <garrettblankenship2002@gmail.com>
2023-07-24 10:26:16 -0700

    No message

--- automation /image_folder_convert.py
+++ automation /image_folder_convert.py
@@ -9,6 +9,10 @@ for filename in os.listdir(path):
     
     edit = img.filter(ImageFiter.SHARPEN).convert('L').rotate(-90)
     
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    
     clean_name = os.path.splitext(filename)[0]
     
     edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
